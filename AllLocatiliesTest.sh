539fdc468bc47cc39ff027a0db654a8db420bb9a
DenysSenkov <denys.senkov@intellias.com>
2022-03-25 11:35:05 +0400

    bash script

---
+++ AllLocatiliesTest.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+mvn test -Dtest.baseURI=http://localhost:8080 -Dtest.configurationId=r246 -Dtest=TestAdapterAllPossibleTrips -DsearchTripSync=false -Dbindings=true -Dseatmap=false -Ddate=2022-03-25 \
+& mvn test -Dtest.baseURI=http://localhost:8080 -Dtest.configurationId=r246 -Dtest=TestAdapterAllPossibleTrips -DsearchTripSync=false -Dbindings=true -Dseatmap=false -Ddate=2022-03-26 \
+& mvn test -Dtest.baseURI=http://localhost:8080 -Dtest.configurationId=r246 -Dtest=TestAdapterAllPossibleTrips -DsearchTripSync=false -Dbindings=true -Dseatmap=false -Ddate=2022-03-27 \
+& mvn test -Dtest.baseURI=http://localhost:8080 -Dtest.configurationId=r246 -Dtest=TestAdapterAllPossibleTrips -DsearchTripSync=false -Dbindings=true -Dseatmap=false -Ddate=2022-03-28 \
+& mvn test -Dtest.baseURI=http://localhost:8080 -Dtest.configurationId=r246 -Dtest=TestAdapterAllPossibleTrips -DsearchTripSync=false -Dbindings=true -Dseatmap=false -Ddate=2022-03-29 \
+& mvn test -Dtest.baseURI=http://localhost:8080 -Dtest.configurationId=r246 -Dtest=TestAdapterAllPossibleTrips -DsearchTripSync=false -Dbindings=true -Dseatmap=false -Ddate=2022-03-30 \
+& mvn test -Dtest.baseURI=http://localhost:8080 -Dtest.configurationId=r246 -Dtest=TestAdapterAllPossibleTrips -DsearchTripSync=false -Dbindings=true -Dseatmap=false -Ddate=2022-03-31 \
+& mvn test -Dtest.baseURI=http://localhost:8080 -Dtest.configurationId=r246 -Dtest=TestAdapterAllPossibleTrips -DsearchTripSync=false -Dbindings=true -Dseatmap=false -Ddate=2022-04-01 \
+& mvn test -Dtest.baseURI=http://localhost:8080 -Dtest.configurationId=r246 -Dtest=TestAdapterAllPossibleTrips -DsearchTripSync=false -Dbindings=true -Dseatmap=false -Ddate=2022-04-02 \
+& mvn test -Dtest.baseURI=http://localhost:8080 -Dtest.configurationId=r246 -Dtest=TestAdapterAllPossibleTrips -DsearchTripSync=false -Dbindings=true -Dseatmap=false -Ddate=2022-04-03 \
+& mvn test -Dtest.baseURI=http://localhost:8080 -Dtest.configurationId=r246 -Dtest=TestAdapterAllPossibleTrips -DsearchTripSync=false -Dbindings=true -Dseatmap=false -Ddate=2022-04-04 \
+& mvn test -Dtest.baseURI=http://localhost:8080 -Dtest.configurationId=r246 -Dtest=TestAdapterAllPossibleTrips -DsearchTripSync=false -Dbindings=true -Dseatmap=false -Ddate=2022-04-05 \
+& mvn test -Dtest.baseURI=http://localhost:8080 -Dtest.configurationId=r246 -Dtest=TestAdapterAllPossibleTrips -DsearchTripSync=false -Dbindings=true -Dseatmap=false -Ddate=2022-04-06 \
+& mvn test -Dtest.baseURI=http://localhost:8080 -Dtest.configurationId=r246 -Dtest=TestAdapterAllPossibleTrips -DsearchTripSync=false -Dbindings=true -Dseatmap=false -Ddate=2022-04-07 \
+& mvn test -Dtest.baseURI=http://localhost:8080 -Dtest.configurationId=r246 -Dtest=TestAdapterAllPossibleTrips -DsearchTripSync=false -Dbindings=true -Dseatmap=false -Ddate=2022-04-08
